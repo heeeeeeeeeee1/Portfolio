1ecc09dd3fc8f89866e44dfa3c12c2bb7307575a
zoe <ddu_buk_e@naver.com>
2024-12-22 02:14:52 +0900

    feat: S-Core Dream 폰트 적용

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import '../public/fonts/fonts.css';
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
